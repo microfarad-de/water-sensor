d0a40b06a6937b4b4dbbe36271159dc8ca73455b
Karim Hraibi <khraibi@apple.com>
2023-05-07 18:18:38 +0200

    Updated release date

--- water-sensor.ino
+++ water-sensor.ino
@@ -39,7 +39,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Version: 1.0.0
- * Date:    March 2022
+ * Date:    May 07, 2023
  */
 #define VERSION_MAJOR 1  // Major version
 #define VERSION_MINOR 0  // Minor version
